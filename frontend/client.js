6b3fffbc958266b43ac457ae9eceea884cbafb20
lucasribolli <lucasribolli@gmail.com>
2023-02-18 16:44:17 -0300

    first commit

---
+++ frontend/client.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws');
+
+// const serverAddress = "ws://127.0.0.1:5000";
+// const serverAddress = 'wss://simple-websocket-server-echo.glitch.me/';
+const serverAddress = 'wss://fierce-absorbed-cuckoo.glitch.me';
+
+const ws = new WebSocket(serverAddress, {
+    headers: {
+        "user-agent": "Mozilla"
+    }
+});
+
+ws.on('open', function() {
+    ws.send("[" + new Date() + "] " + "Oi, sou um Client e quero receber dados em real-time.");
+});
+
+ws.on('message', function(msg) {
+    console.log("Received msg from the server: " + msg);
+});
